e1350516d686b0b293e5c74111daa6748179b4a0
mizzi <56218572+mizzi47@users.noreply.github.com>
2022-12-22 01:57:02 +0800

    update new version

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,10 @@ class _MyAppState extends State<MyApp> {
   bool _isListening = false;
 
   String transcription = '';
+  int listcount1 = 0;
+  int listcount2 = 0;
+  int listcount3 = 0;
+
   int count1 = 0;
   int count2 = 0;
   int count3 = 0;
@@ -61,60 +65,130 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        extendBodyBehindAppBar: true,
         appBar: AppBar(
-          title: Text('SpeechRecognition'),
+          backgroundColor: Colors.greenAccent.withOpacity(0.5),
+          title: Center(child: Text('Qul Tasbih')),
           actions: [
-            PopupMenuButton<Language>(
-              onSelected: _selectLangHandler,
-              itemBuilder: (BuildContext context) => _buildLanguagesWidgets,
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent.withOpacity(0.5)),
+                onPressed: (){
+                  setState(() {
+                    count1 = 0;
+                    count2 = 0;
+                    count3 = 0;
+                    transcription = "";
+                    listcount3 = 0;
+                  });
+                },
+                child: Icon(Icons.refresh)
             )
           ],
         ),
-        body: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                      child: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          color: Colors.grey.shade200,
-                          child: Text(transcription))),
-                  Expanded(
-                      child: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          color: Colors.grey.shade200,)),
-                  _buildButton(
-                    onPressed: _speechRecognitionAvailable && !_isListening
-                        ? () => start()
-                        : null,
-                    label: _isListening
-                        ? 'Listening...'
-                        : 'Start Zikr',
-                  ),
-                  // _buildButton(
-                  //   onPressed: _isListening ? () => cancel() : null,
-                  //   label: 'Cancel',
-                  // ),
-                  _buildButton(
-                    onPressed: _isListening ? () => stop() : null,
-                    label: 'Stop',
-                  ),
-                  _buildButton(
-                    label: 'Subhanallah : $count1',
-                  ),
-                  _buildButton(
-                    label: 'Alhamdulillah : $count2',
-                  ),
-                  _buildButton(
-                    label: 'Allahuakbar : $count3',
-                  ),
-                ],
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/bg.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: 100,),
+                    Expanded(
+                        child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            color: Colors.grey.shade200.withOpacity(0.5),
+                            child: Text(transcription))),
+                    _buildButton(
+                      onPressed: _speechRecognitionAvailable && !_isListening
+                          ? () => start()
+                          : null,
+                      label: _isListening
+                          ? 'Listening...'
+                          : 'Start Zikr',
+                      cl: Colors.green
+                    ),
+                    // _buildButton(
+                    //   onPressed: _isListening ? () => cancel() : null,
+                    //   label: 'Cancel',
+                    // ),
+                    _stopButton(
+                      onPressed: _isListening ? () => stop() : null,
+                      label: 'Stop',
+                        cl: Colors.red
+                    ),
+                    //checking and counted zikr label
+                    Row(
+                      children: [
+                        Expanded(
+                          child: _buildButton(
+                            label: 'Subhanallah : $count1',
+                              cl: Colors.greenAccent
+                          ),
+                        ),
+                        Container(
+                          child: ElevatedButton(
+                            onPressed: (){
+                              setState(() {
+                                count1 = 0;
+                              });
+                            },
+                            child: Icon(Icons.refresh)
+                          )
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: _buildButton(
+                            label: 'Alhamdulillah : $count2',
+                              cl: Colors.greenAccent
+                          ),
+                        ),
+                        Container(
+                            child: ElevatedButton(
+                                onPressed: (){
+                                  setState(() {
+                                    count2 = 0;
+                                  });
+                                },
+                                child: Icon(Icons.refresh)
+                            )
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: _buildButton(
+                            label: 'Allahuakbar : $count3',
+                            cl: Colors.greenAccent
+                          ),
+                        ),
+                        Container(
+                            child: ElevatedButton(
+                                onPressed: (){
+                                  setState(() {
+                                    count3 = 0;
+                                  });
+                                },
+                                child: Icon(Icons.refresh)
+                            )
+                        ),
+                      ],
+                    ),
+                  ],
 
-              ),
-            )),
+                ),
+              )),
+        ),
       ),
     );
   }
@@ -131,22 +205,28 @@ class _MyAppState extends State<MyApp> {
     setState(() => selectedLang = lang);
   }
 
-  Widget _buildButton({required String label, VoidCallback? onPressed}) => Padding(
+  Widget _buildButton({required String label, VoidCallback? onPressed, required Color cl}) => Padding(
       padding: EdgeInsets.all(12.0),
-      child: ElevatedButton(
+      child: MaterialButton(
         onPressed: onPressed,
+        color: cl,
+        disabledColor: cl.withOpacity(0.5),
         child: Text(
           label,
           style: const TextStyle(color: Colors.white),
         ),
       ));
-  Widget _countButton({required String label, VoidCallback? onPressed}) => Padding(
+
+  Widget _stopButton({required String label, VoidCallback? onPressed, required Color cl}) => Padding(
       padding: EdgeInsets.all(12.0),
       child: ElevatedButton(
         onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.red
+        ),
         child: Text(
           label,
-          style: const TextStyle(color: Colors.green),
+          style: const TextStyle(color: Colors.white),
         ),
       ));
 
@@ -166,9 +246,6 @@ class _MyAppState extends State<MyApp> {
   void stop() => _speech.stop().then((_) {
     setState(() {
       _isListening = false;
-      count1=0;
-      count2=0;
-      count3=0;
     });
   });
 
@@ -185,6 +262,7 @@ class _MyAppState extends State<MyApp> {
     setState(() => _isListening = true);
   }
 
+  //process of breaking voice into words for counting
   void onRecognitionResult(String text) {
     print('_MyAppState.onRecognitionResult... $text');
     setState(() => transcription = text);
@@ -194,6 +272,10 @@ class _MyAppState extends State<MyApp> {
     setState(() => count1 = s.length-1);
     setState(() => count2 = al.length-1);
     setState(() => count3 = aa.length-1);
+    if(_isListening == false){
+      listcount3 += count3;
+    }
+    print(listcount3);
   }
 
   void onRecognitionComplete(String text) {
